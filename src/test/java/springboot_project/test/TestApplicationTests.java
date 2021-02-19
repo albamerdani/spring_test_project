bf8c1bf521a5bd25fb4fdb5ff2af6fbe96469461
U165940 <alba.merdani@lhind.dlh.de>
2021-02-19 12:06:51 +0100

    Initial commit

---
+++ src/test/java/springboot_project/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_project.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
