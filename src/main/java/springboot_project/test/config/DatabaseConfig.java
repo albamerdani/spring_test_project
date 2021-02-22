f5088ab3510084d1175b8fef44b37009751798ed
U165940 <alba.merdani@lhind.dlh.de>
2021-02-22 21:46:08 +0100

    Fixing dependencies and imports

--- src/main/java/springboot_project/test/config/DatabaseConfig.java
+++ src/main/java/springboot_project/test/config/DatabaseConfig.java
@@ -1,14 +1,12 @@
 package springboot_project.test.config;
-import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
@@ -19,11 +17,13 @@ public class DatabaseConfig {
         public DataSource createDataSource() {
             return DataSourceBuilder.create().build();
         }
+/*
 
         @Bean(name = "jdbcService")
         @Autowired
         public JdbcTemplate createJdbcTemplate_Service(@Qualifier("dbService") DataSource productServiceDS) {
             return new JdbcTemplate(productServiceDS);
         }
+*/
 
 }
