f37122f7a90a5bf1371f1e7954afe4f33b24436e
U165940 <alba.merdani@lhind.dlh.de>
2021-02-28 00:28:55 +0100

    Changes to fix jpa exception

--- src/main/java/springboot_project/test/TestApplication.java
+++ src/main/java/springboot_project/test/TestApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"springboot_project.test.model", "springboot_project.test.controller", "springboot_project.test.repository", "springboot_project.test.config"})
+@ComponentScan("springboot_project.test")
 @EnableJpaAuditing
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = "springboot_project.test.repository")
 public class TestApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(TestApplication.class);
